3a62b48d775e29b5611c385bf1f5553158ba5d1e
G.Garci4 <66149021+OCobr4@users.noreply.github.com>
2020-07-19 22:30:21 -0300

    Iniciando o jogo.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import sys
+import pygame
 
+# Inicaliza o jogo e cria um objeto para a tela.
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode(1200, 800)
+    pygame.display.set_caption("Alien Invasion")
+
+# Iniciar o laço principal do jogo.
+
+
+while True:
+    # Observa eventos do teclado e de mouse.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # Deixa a tela mais recente visivel.
+    pygame.display.flip()
+
+run_game()
